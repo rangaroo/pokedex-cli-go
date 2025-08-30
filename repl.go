e1932d45ff6521e1a71c539ca80ffcf7b1cdc69f
Aslan Onalbek <aslan.onalbek@nu.edu.kz>
2025-08-30 21:04:23 +0500

    Added 'help' and 'exit' commands

--- repl.go
+++ repl.go
@@ -6,23 +6,62 @@ import (
 	"bufio"
 	"os"
 )
-func cleanInput(text string) []string {
-	lowered := strings.ToLower(text)
-	return strings.Fields(lowered)
-}
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		
-		text := cleanInput(scanner.Text())
-		
-		fmt.Printf("Your command was: %s\n", text[0])
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
+
+func cleanInput(text string) []string {
+	lowered := strings.ToLower(text)
+	words := strings.Fields(lowered)
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
 }
